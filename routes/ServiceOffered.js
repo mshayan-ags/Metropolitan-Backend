92fee90f35b3c0f3074d605fba6176d313d156a9
mshayan-ags <mshayan4002@gmail.com>
2023-08-09 01:41:47 +0500

    Fixed serviceOffered Routes Small Issues

--- routes/ServiceOffered.js
+++ routes/ServiceOffered.js
@@ -1,19 +1,33 @@
 const { ServiceOffered } = require("../models/ServiceOffered");
 const { getAdminId } = require("../utils/AuthCheck");
 const { Router } = require("express");
-const { filterArrayOfObjectAndRemoveRepetitions } = require("../utils/functions");
+const {
+  filterArrayOfObjectAndRemoveRepetitions,
+  CheckAllRequiredFieldsAvailaible,
+} = require("../utils/functions");
 
 const router = Router();
 
 router.post("/Create-ServiceOffered", async (req, res) => {
   try {
-    const { id, message } = getAdminId(req);
+    const { id, message } = await getAdminId(req);
     if (id) {
       const Credentials = req.body;
 
-      console.log(Credentials?.Fields);
+      const Check = CheckAllRequiredFieldsAvailaible(
+        Credentials,
+        ["Fields", "title"],
+        res
+      );
+      if (Check == "Error") {
+        return;
+      }
+
       const FieldsArr = JSON.parse(Credentials?.Fields);
-      const newFieldsArr = filterArrayOfObjectAndRemoveRepetitions(FieldsArr, "name");
+      const newFieldsArr = filterArrayOfObjectAndRemoveRepetitions(
+        FieldsArr,
+        "name"
+      );
 
       const newServiceOffered = new ServiceOffered({
         title: Credentials?.title,
@@ -30,7 +44,6 @@ router.post("/Create-ServiceOffered", async (req, res) => {
       res.status(401).json({ status: 401, message: message });
     }
   } catch (error) {
-    console.log(error);
     if (error?.code == 11000) {
       res.status(500).json({
         status: 500,
@@ -46,57 +59,68 @@ router.post("/Create-ServiceOffered", async (req, res) => {
 
 router.post("/Update-ServiceOffered", async (req, res) => {
   try {
-    const { id, message } = getAdminId(req);
+    const { id, message } = await getAdminId(req);
     if (id) {
       const Credentials = req.body;
 
-      ServiceOffered.findOne({ _id: req.body.id })
-        .then(async (data) => {
-          const FieldsArr = [
-            ...JSON.parse(Credentials?.Fields),
-            ...data?.Fields,
-          ];
-          const newFieldsArr = filterArrayOfObjectAndRemoveRepetitions(
-            FieldsArr,
-            "name"
-          );
+      const Check = CheckAllRequiredFieldsAvailaible(Credentials, ["id"], res);
+      if (Check == "Error") {
+        return;
+      }
 
-          await ServiceOffered.updateOne(
-            { _id: data?._id },
-            {
-              title: Credentials?.title ? Credentials?.title : data?.title,
-              Fields: newFieldsArr,
-            },
-            {
-              new: false,
-            }
-          )
-            .then((docs) => {
-              res.status(401).json({
-                status: 200,
-                message: "Your ServiceOffered has been Updated",
-              });
-            })
-            .catch((error) => {
-              console.log(error);
-              res.status(500).json({ status: 500, message: error });
+      const searchServiceOffered = await ServiceOffered.findOne({
+        _id: Credentials.id,
+      });
+
+      if (searchServiceOffered?._id) {
+        const FieldsArr = [
+          ...JSON.parse(Credentials?.Fields),
+          ...searchServiceOffered?.Fields,
+        ];
+        const newFieldsArr = filterArrayOfObjectAndRemoveRepetitions(
+          FieldsArr,
+          "name"
+        );
+
+        await ServiceOffered.updateOne(
+          { _id: searchServiceOffered?._id },
+          {
+            title: Credentials?.title
+              ? Credentials?.title
+              : searchServiceOffered?.title,
+            Fields: newFieldsArr,
+          },
+          {
+            new: false,
+          }
+        )
+          .then((docs) => {
+            res.status(200).json({
+              status: 200,
+              message: "Your ServiceOffered has been Updated",
             });
-        })
-        .catch((error) => {
-          console.log(error);
-          res.status(500).json({ status: 500, message: error });
-        });
+          })
+          .catch((error) => {
+            console.log(error);
+            res.status(500).json({ status: 500, message: error });
+          });
+      } else {
+        res.status(500).json({ status: 500, message: error });
+      }
     } else {
       res.status(401).json({ status: 401, message: message });
     }
   } catch (error) {
-    console.log(error);
     res.status(500).json({ status: 500, message: error });
   }
 });
 
 router.get("/ServiceOfferedInfo/:id", async (req, res) => {
   try {
+    const Check = CheckAllRequiredFieldsAvailaible(req?.params, ["id"], res);
+    if (Check == "Error") {
+      return;
+    }
     ServiceOffered.findOne({ _id: req.params.id })
       .populate("Icon")
       .then((data) => {
