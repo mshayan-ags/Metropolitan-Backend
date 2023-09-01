304949df8a4b1abf60806a21d0cf8f4123400ef5
mshayan-ags <mshayan4002@gmail.com>
2023-09-02 00:03:46 +0500

    Data Entry

---
+++ models/DataEntry.js
@@ -0,0 +1,94 @@
+const mongoose = require("mongoose");
+
+const DataEntrySchema = new mongoose.Schema(
+  {
+    IC: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+    },
+    SAP: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    INB: {
+      type: String,
+      required: true,
+    },
+    BL: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    Weight: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    Package: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+    },
+    Supplier: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    Description: {
+      type: String,
+      required: true,
+    },
+    Inv: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    ShippingPort: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    ShippingLine: {
+      type: String,
+      required: true,
+    },
+    BLDate: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    Terminal: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    DocreeDate: {
+      type: String,
+      required: true,
+    },
+    ShipBy: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    TypeOfDocument: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+  },
+  {
+    timestamps: {
+      createdAt: "created_at", // Use `created_at` to store the created date
+      updatedAt: "updated_at", // and `updated_at` to store the last updated date
+    },
+  }
+);
+
+const DataEntry = mongoose.model("DataEntry", DataEntrySchema);
+
+module.exports = DataEntry;
---
+++ routes/DataEntry.js
@@ -0,0 +1,162 @@
+const { DataEntry } = require("../models/DataEntry");
+const { getUserId, getUserId } = require("../utils/AuthCheck");
+const { Router } = require("express");
+const { CheckAllRequiredFieldsAvailaible } = require("../utils/functions");
+const { connectToDB } = require("../Middlewares/Db");
+const router = Router();
+
+router.post("/Create-DataEntry", async (req, res) => {
+  try {
+    connectToDB();
+
+    const { id, message } = await getUserId(req);
+    if (id) {
+      const Credentials = req.body;
+
+      const Check = await CheckAllRequiredFieldsAvailaible(
+        Credentials,
+        [
+          "IC",
+          "SAP",
+          "INB",
+          "BL",
+          "Weight",
+          "Package",
+          "Supplier",
+          "Description",
+          "Inv",
+          "ShippingPort",
+          "ShippingLine",
+          "BLDate",
+          "Terminal",
+          "DocreeDate",
+          "ShipBy",
+          "TypeOfDocument",
+        ],
+        res
+      );
+      if (Check == "Error") {
+        return;
+      }
+
+      const newDataEntry = new DataEntry(Credentials);
+
+      const ImgArr = JSON.parse(Credentials?.images);
+
+      await newDataEntry.save();
+
+      res.status(200).json({
+        status: 200,
+        message: "DataEntry Created in Succesfully",
+      });
+    } else {
+      res.status(401).json({ status: 401, message: message });
+    }
+  } catch (error) {
+    if (error?.code == 11000) {
+      res.status(500).json({
+        status: 500,
+        message: `Please Change your ${
+          Object.keys(error?.keyValue)[0]
+        } as it's not unique`,
+      });
+    } else {
+      res.status(500).json({ status: 500, message: error });
+    }
+  }
+});
+
+router.post("/Update-DataEntry", async (req, res) => {
+  try {
+    connectToDB();
+
+    const { id, message } = await getUserId(req);
+    if (id) {
+      const Credentials = req.body;
+
+      const Check = await CheckAllRequiredFieldsAvailaible(
+        Credentials,
+        ["id"],
+        res
+      );
+      if (Check == "Error") {
+        return;
+      }
+
+      const searchDataEntry = await DataEntry.findOne({ _id: Credentials.id });
+      if (searchDataEntry?._id) {
+        await DataEntry.updateOne({ _id: data?._id }, Credentials, {
+          new: false,
+        })
+          .then((docs) => {
+            res.status(200).json({
+              status: 200,
+              message: "Your DataEntry has been Updated",
+            });
+          })
+          .catch((error) => {
+            res.status(500).json({ status: 500, message: error });
+          });
+      } else {
+        res.status(500).json({ status: 500, message: "DataEntry not Found" });
+      }
+    } else {
+      res.status(401).json({ status: 401, message: message });
+    }
+  } catch (error) {
+    res.status(500).json({ status: 500, message: error });
+  }
+});
+
+router.get("/DataEntryInfo/:id", async (req, res) => {
+  try {
+    connectToDB();
+    const { id, message } = await getUserId(req);
+    if (id) {
+      const Check = await CheckAllRequiredFieldsAvailaible(
+        req.params,
+        ["id"],
+        res
+      );
+      if (Check == "Error") {
+        return;
+      }
+
+      DataEntry.findOne({ _id: req.params.id })
+        .then((data) => {
+          res.status(200).json({ status: 200, data: data });
+        })
+        .catch((err) => {
+          res.status(500).json({ status: 500, message: err });
+        });
+    } else {
+      res.status(401).json({ status: 401, message: message });
+    }
+  } catch (error) {
+    res.status(500).json({ status: 500, message: error });
+  }
+});
+
+router.get("/GetAllDataEntry", async (req, res) => {
+  try {
+    connectToDB();
+    const { id, message } = await getUserId(req);
+    const { id: userId, message: userMessage } = await getUserId(req);
+
+    if (id || userId) {
+      DataEntry.find()
+        .then((data) => {
+          res.status(200).json({ status: 200, data: data });
+        })
+        .catch((err) => {
+          res.status(500).json({ status: 500, message: err });
+        });
+    } else {
+      res.status(401).json({ status: 401, message: message || userMessage });
+    }
+  } catch (error) {
+    res.status(500).json({ status: 500, message: error });
+  }
+});
+
+module.exports = router;
