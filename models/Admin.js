16de6fc0acf48376747b20837ee0924fd7307731
mshayan-ags <mshayan.ags@gmail.com>
2024-03-09 06:52:25 +0500

    new Role         reception

--- models/Admin.js
+++ models/Admin.js
@@ -1,78 +1,83 @@
 const mongoose = require("mongoose");
 
 const AdminSchema = new mongoose.Schema(
-  {
-    name: {
-      type: String,
-      required: true,
-      trim: true,
-    },
-    email: {
-      type: String,
-      required: true,
-      trim: true,
-      lowercase: true,
-      unique: true,
-    },
-    phoneNumber: {
-      type: Number,
-      required: true,
-      default: 0,
-      unique: true,
-    },
-    password: {
-      type: String,
-      required: true,
-      trim: true,
-    },
-    Role: {
-      type: String,
-      required: true,
-    },
-    Responsiblities: {
-      type: Map,
-      of: Boolean,
-    },
-    profilePicture: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Image",
-    },
-    Service: {
-      type: [mongoose.Schema.Types.ObjectId],
-      ref: "Service",
-    },
-    Event: {
-      type: [mongoose.Schema.Types.ObjectId],
-      ref: "Event",
-    },
-    Notification: {
-      type: [mongoose.Schema.Types.ObjectId],
-      ref: "Notification",
-    },
-    Complain: {
-      type: [mongoose.Schema.Types.ObjectId],
-      ref: "Complain",
-    },
-    Chat: {
-      type: [mongoose.Schema.Types.ObjectId],
-      ref: "Chat",
-    },
-    Message: {
-      type: [mongoose.Schema.Types.ObjectId],
-      ref: "Message",
-    },
-    Tasks: {
-      type: [mongoose.Schema.Types.ObjectId],
-      ref: "Task",
-    },
-
-  },
-  {
-    timestamps: {
-      createdAt: "created_at", // Use `created_at` to store the created date
-      updatedAt: "updated_at", // and `updated_at` to store the last updated date
-    },
-  }
+	{
+		name: {
+			type: String,
+			required: true,
+			trim: true
+		},
+		email: {
+			type: String,
+			required: true,
+			trim: true,
+			lowercase: true,
+			unique: true
+		},
+		phoneNumber: {
+			type: Number,
+			required: true,
+			default: 0,
+			unique: true
+		},
+		password: {
+			type: String,
+			required: true,
+			trim: true
+		},
+		Role: {
+			type: String,
+			required: true,
+			enum: ["superadmin", "admin", "manager", "user", "reception"]
+		},
+		TowerNo: {
+			type: Number,
+			required: true,
+			trim: true
+		},
+		Responsiblities: {
+			type: Map,
+			of: Boolean
+		},
+		profilePicture: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "Image"
+		},
+		Service: {
+			type: [mongoose.Schema.Types.ObjectId],
+			ref: "Service"
+		},
+		Event: {
+			type: [mongoose.Schema.Types.ObjectId],
+			ref: "Event"
+		},
+		Notification: {
+			type: [mongoose.Schema.Types.ObjectId],
+			ref: "Notification"
+		},
+		Complain: {
+			type: [mongoose.Schema.Types.ObjectId],
+			ref: "Complain"
+		},
+		Chat: {
+			type: [mongoose.Schema.Types.ObjectId],
+			ref: "Chat"
+		},
+		Message: {
+			type: [mongoose.Schema.Types.ObjectId],
+			ref: "Message"
+		},
+		Tasks: {
+			type: [mongoose.Schema.Types.ObjectId],
+			ref: "Task"
+		}
+	},
+	{
+		timestamps: {
+			createdAt: "created_at", // Use `created_at` to store the created date
+			updatedAt: "updated_at" // and `updated_at` to store the last updated date
+		}
+	}
 );
 
 const Admin = mongoose.model("Admin", AdminSchema);
--- routes/Admin.js
+++ routes/Admin.js
@@ -11,230 +11,212 @@ const { default: mongoose } = require("mongoose");
 const router = Router();
 
 router.post("/Create-Admin", async (req, res) => {
-  try {
-    connectToDB();
+	try {
+		connectToDB();
 
-    const Credentials = req.body;
+		const Credentials = req.body;
 
-    const Check = await CheckAllRequiredFieldsAvailaible(
-      Credentials,
-      ["name", "email", "phoneNumber", "Role", "password"],
-      res
-    );
-    if (Check) {
-      return;
-    }
+		const Check = await CheckAllRequiredFieldsAvailaible(
+			Credentials,
+			["name", "email", "phoneNumber", "Role", "password", "TowerNo"],
+			res
+		);
+		if (Check) {
+			return;
+		}
 
-    const password = await bcrypt.hash(Credentials?.password, 15);
+		const password = await bcrypt.hash(Credentials?.password, 15);
 
-    const newAdmin = new Admin({
-      name: Credentials?.name,
-      email: Credentials?.email,
-      phoneNumber: Credentials?.phoneNumber,
-      password: password,
-      Role: Credentials?.Role,
-      // Responsiblities: Credentials.Responsiblities,
-    });
+		const newAdmin = new Admin({
+			name: Credentials?.name,
+			email: Credentials?.email,
+			phoneNumber: Credentials?.phoneNumber,
+			password: password,
+			Role: Credentials?.Role,
+			TowerNo: Credentials?.TowerNo
+			// Responsiblities: Credentials.Responsiblities,
+		});
 
-    if (Credentials?.profilePicture?.name) {
-      const image = await SaveImageDB(
-        Credentials?.profilePicture,
-        { Admin: new mongoose.Types.ObjectId(newAdmin?._id) },
-        res
-      );
+		if (Credentials?.profilePicture?.name) {
+			const image = await SaveImageDB(
+				Credentials?.profilePicture,
+				{ Admin: new mongoose.Types.ObjectId(newAdmin?._id) },
+				res
+			);
 
-      if (image?.file?._id) {
-        newAdmin.profilePicture = new mongoose.Types.ObjectId(image?.file?._id);
-      } else {
-        res.status(500).json({ status: 500, message: image?.Error });
-      }
-    }
-    await newAdmin.save();
-    const token = jwt.sign(
-      { id: newAdmin?._id, Role: newAdmin?.Role },
-      APP_SECRET
-    );
+			if (image?.file?._id) {
+				newAdmin.profilePicture = new mongoose.Types.ObjectId(image?.file?._id);
+			} else {
+				res.status(500).json({ status: 500, message: image?.Error });
+			}
+		}
+		await newAdmin.save();
+		const token = jwt.sign({ id: newAdmin?._id, Role: newAdmin?.Role }, APP_SECRET);
 
-    res.status(200).json({
-      token,
-      status: 200,
-      message: "Admin Created in Succesfully",
-    });
-  } catch (error) {
-    if (error?.code == 11000) {
-      res.status(500).json({
-        status: 500,
-        message: `Please Change your ${
-          Object.keys(error?.keyValue)[0]
-        } as it's not unique`,
-      });
-    } else {
-      res.status(500).json({ status: 500, message: error });
-    }
-  }
+		res.status(200).json({
+			token,
+			status: 200,
+			message: "Admin Created in Succesfully"
+		});
+	} catch (error) {
+		if (error?.code == 11000) {
+			res.status(500).json({
+				status: 500,
+				message: `Please Change your ${Object.keys(error?.keyValue)[0]} as it's not unique`
+			});
+		} else {
+			res.status(500).json({ status: 500, message: error });
+		}
+	}
 });
 
 router.post("/Update-Admin", async (req, res) => {
-  try {
-    connectToDB();
+	try {
+		connectToDB();
 
-    const { id, message } = await getAdminId(req);
-    if (id) {
-      const Credentials = req.body;
+		const { id, message } = await getAdminId(req);
+		if (id) {
+			const Credentials = req.body;
 
-      const searchAdmin = await Admin.findOne({ _id: id });
+			const searchAdmin = await Admin.findOne({ _id: id });
 
-      if (searchAdmin?._id) {
-        if (Credentials?.profilePicture?.name) {
-          const image = await SaveImageDB(
-            Credentials?.profilePicture,
-            { Admin: new mongoose.Types.ObjectId(searchAdmin?._id) },
-            res
-          );
-          if (image?.file?._id) {
-            Credentials.profilePicture = new mongoose.Types.ObjectId(
-              image?.file?._id
-            );
-          } else {
-            res.status(500).json({ status: 500, message: image?.Error });
-          }
-        }
-        await Admin.updateOne({ _id: id }, Credentials, {
-          new: false,
-        })
-          .then((docs) => {
-            res.status(401).json({
-              status: 200,
-              message: "Your Admin has been Updated",
-            });
-          })
-          .catch((error) => {
-            res.status(500).json({ status: 500, message: error });
-          });
-      } else {
-        res.status(500).json({ status: 500, message: "Admin Not Found" });
-      }
-    } else {
-      res.status(401).json({ status: 401, message: message });
-    }
-  } catch (error) {
-    res.status(500).json({ status: 500, message: error });
-  }
+			if (searchAdmin?._id) {
+				if (Credentials?.profilePicture?.name) {
+					const image = await SaveImageDB(
+						Credentials?.profilePicture,
+						{ Admin: new mongoose.Types.ObjectId(searchAdmin?._id) },
+						res
+					);
+					if (image?.file?._id) {
+						Credentials.profilePicture = new mongoose.Types.ObjectId(image?.file?._id);
+					} else {
+						res.status(500).json({ status: 500, message: image?.Error });
+					}
+				}
+				await Admin.updateOne({ _id: id }, Credentials, {
+					new: false
+				})
+					.then((docs) => {
+						res.status(401).json({
+							status: 200,
+							message: "Your Admin has been Updated"
+						});
+					})
+					.catch((error) => {
+						res.status(500).json({ status: 500, message: error });
+					});
+			} else {
+				res.status(500).json({ status: 500, message: "Admin Not Found" });
+			}
+		} else {
+			res.status(401).json({ status: 401, message: message });
+		}
+	} catch (error) {
+		res.status(500).json({ status: 500, message: error });
+	}
 });
 
 router.post("/Login-Admin", async (req, res) => {
-  try {
-    connectToDB();
+	try {
+		connectToDB();
 
-    const Credentials = req.body;
+		const Credentials = req.body;
 
-    const Check = await CheckAllRequiredFieldsAvailaible(
-      Credentials,
-      ["email", "password"],
-      res
-    );
-    if (Check) {
-      return;
-    }
+		const Check = await CheckAllRequiredFieldsAvailaible(Credentials, ["email", "password"], res);
+		if (Check) {
+			return;
+		}
 
-    const searchAdmin = await Admin.findOne({ email: Credentials?.email });
+		const searchAdmin = await Admin.findOne({ email: Credentials?.email });
 
-    if (searchAdmin?.password && searchAdmin?._id) {
-      const valid = await bcrypt.compare(
-        Credentials?.password,
-        searchAdmin?.password
-      );
+		if (searchAdmin?.password && searchAdmin?._id) {
+			const valid = await bcrypt.compare(Credentials?.password, searchAdmin?.password);
 
-      if (valid) {
-        const token = jwt.sign(
-          { id: searchAdmin?._id, Role: searchAdmin?.Role },
-          APP_SECRET
-        );
-        res.status(200).json({
-          token,
-          status: 200,
-          message: "Admin Logged in Succesfully",
-        });
-      } else if (!valid) {
-        res
-          .status(500)
-          .json({ status: 500, message: "Your Password is incorrect" });
-      } else {
-        res.status(500).json({ status: 500, message: "Admin Not Verified" });
-      }
-    } else {
-      res.status(500).json({ status: 500, message: "Admin Not Found" });
-    }
-  } catch (error) {
-    res.status(500).json({ status: 500, message: error });
-  }
+			if (valid) {
+				const token = jwt.sign({ id: searchAdmin?._id, Role: searchAdmin?.Role }, APP_SECRET);
+				res.status(200).json({
+					token,
+					status: 200,
+					message: "Admin Logged in Succesfully"
+				});
+			} else if (!valid) {
+				res.status(500).json({ status: 500, message: "Your Password is incorrect" });
+			} else {
+				res.status(500).json({ status: 500, message: "Admin Not Verified" });
+			}
+		} else {
+			res.status(500).json({ status: 500, message: "Admin Not Found" });
+		}
+	} catch (error) {
+		res.status(500).json({ status: 500, message: error });
+	}
 });
 
 router.get("/AdminInfo", async (req, res) => {
-  try {
-    connectToDB();
+	try {
+		connectToDB();
 
-    const { id, message } = await getAdminId(req);
-    if (id) {
-      Admin.findOne({ _id: id })
-        .then((data) => {
-          res.status(200).json({ status: 200, data: data });
-        })
-        .catch((err) => {
-          res.status(500).json({ status: 500, message: err });
-        });
-    } else {
-      res.status(401).json({ status: 401, message: message });
-    }
-  } catch (error) {
-    res.status(500).json({ status: 500, message: error });
-  }
+		const { id, message } = await getAdminId(req);
+		if (id) {
+			Admin.findOne({ _id: id })
+				.then((data) => {
+					res.status(200).json({ status: 200, data: data });
+				})
+				.catch((err) => {
+					res.status(500).json({ status: 500, message: err });
+				});
+		} else {
+			res.status(401).json({ status: 401, message: message });
+		}
+	} catch (error) {
+		res.status(500).json({ status: 500, message: error });
+	}
 });
 
 router.get("/GetAdminInfo/:id", async (req, res) => {
-  try {
-    connectToDB();
+	try {
+		connectToDB();
 
-    const { id, message } = await getAdminId(req);
-    if (id) {
-      Admin.findOne({ _id: req?.params?.id })
-        .populate(["profilePicture"])
-        .then((data) => {
-          res.status(200).json({ status: 200, data: data });
-        })
-        .catch((err) => {
-          res.status(500).json({ status: 500, message: err });
-        });
-    } else {
-      res.status(401).json({ status: 401, message: message });
-    }
-  } catch (error) {
-    res.status(500).json({ status: 500, message: error });
-  }
+		const { id, message } = await getAdminId(req);
+		if (id) {
+			Admin.findOne({ _id: req?.params?.id })
+				.populate(["profilePicture"])
+				.then((data) => {
+					res.status(200).json({ status: 200, data: data });
+				})
+				.catch((err) => {
+					res.status(500).json({ status: 500, message: err });
+				});
+		} else {
+			res.status(401).json({ status: 401, message: message });
+		}
+	} catch (error) {
+		res.status(500).json({ status: 500, message: error });
+	}
 });
 
 router.get("/GetAllAdmins", async (req, res) => {
-  try {
-    const { id, message } = await getAdminId(req);
-    if (id) {
-      Admin.find()
-        .populate(["profilePicture"])
-        .exec()
-        .then((data) => {
-          res.status(200).json({ status: 200, data: data });
-        })
-        .catch((err) => {
-          console.log(err);
+	try {
+		const { id, message } = await getAdminId(req);
+		if (id) {
+			Admin.find()
+				.populate(["profilePicture"])
+				.exec()
+				.then((data) => {
+					res.status(200).json({ status: 200, data: data });
+				})
+				.catch((err) => {
+					console.log(err);
 
-          res.status(500).json({ status: 500, message: err });
-        });
-    } else {
-      res.status(401).json({ status: 401, message: message });
-    }
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ status: 500, message: error });
-  }
+					res.status(500).json({ status: 500, message: err });
+				});
+		} else {
+			res.status(401).json({ status: 401, message: message });
+		}
+	} catch (error) {
+		console.log(error);
+		res.status(500).json({ status: 500, message: error });
+	}
 });
 
 module.exports = router;
