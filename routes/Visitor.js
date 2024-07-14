48eb5278837e371c20ac5b2c60df5c7d8de019b2
mshayan-ags <mshayan.ags@gmail.com>
2024-07-14 22:22:35 +0500

    Visitior Exit Time

--- routes/Visitor.js
+++ routes/Visitor.js
@@ -2,7 +2,7 @@ const { Router } = require("express");
 const { Visitor } = require("../models/Visitor"); // Assuming you've created this model
 const { getAdminId, getUserId } = require("../utils/AuthCheck");
 const { connectToDB } = require("../Middlewares/Db");
-const {  CheckAllRequiredFieldsAvailaible } = require("../utils/functions");
+const { CheckAllRequiredFieldsAvailaible } = require("../utils/functions");
 const { default: mongoose } = require("mongoose");
 const { Admin } = require("../models/Admin");
 
@@ -12,24 +12,26 @@ const router = Router();
 router.post("/create-visitor", async (req, res) => {
     try {
         connectToDB();
-        
+
         const { id, message } = await getAdminId(req);
         if (!id) {
             return res.status(401).json({ status: 401, message: message });
         }
-        
+
         const visitorInfo = req.body;
-        const requiredFields = ["Name", "VehicleNumber", "VisitPurpose", "Visiting","TowerNo","AppratmentNo"];
-        
+        const requiredFields = ["Name", "VehicleNumber", "VisitPurpose", "Visiting", "TowerNo", "AppratmentNo"];
+
         const check = await CheckAllRequiredFieldsAvailaible(visitorInfo, requiredFields, res);
         if (check) {
             return;
         }
 
-        const newVisitor = new Visitor({...visitorInfo,
-        Admin:new mongoose.Types.ObjectId(id)});
+        const newVisitor = new Visitor({
+            ...visitorInfo,
+            Admin: new mongoose.Types.ObjectId(id)
+        });
         const savedVisitor = await newVisitor.save();
-        
+
         res.status(200).json({
             status: 200,
             message: "Visitor created successfully",
@@ -40,11 +42,53 @@ router.post("/create-visitor", async (req, res) => {
     }
 });
 
+
+router.post("/Change-ExitTime-Visitor", async (req, res) => {
+    try {
+        connectToDB();
+        const Credentials = req.body;
+
+        const Check = await CheckAllRequiredFieldsAvailaible(
+            Credentials,
+            ["id", "ExitTime"],
+            res
+        );
+        if (Check) {
+            return;
+        }
+
+        const searchVisitor = await Visitor.findOne({ _id: Credentials?.id });
+        if (searchVisitor?._id) {
+            await Visitor.updateOne(
+                { _id: searchVisitor?._id },
+                { ExitTime: Credentials?.ExitTime },
+                {
+                    new: false
+                }
+            )
+                .then((docs) => {
+                    res.status(200).json({
+                        status: 200,
+                        message: "Your ExitTime has been Changed"
+                    });
+                })
+                .catch((error) => {
+                    res.status(500).json({ status: 500, message: error });
+                });
+        } else {
+            res.status(500).json({ status: 500, message: "Visitor Not Found or wrong email" });
+        }
+    } catch (error) {
+        res.status(500).json({ status: 500, message: error });
+    }
+});
+
+
 router.get("/visitorInfo/:id", async (req, res) => {
     try {
         connectToDB();
         const { id } = req.params;
-        
+
         const visitor = await Visitor.findById(id);
         if (!visitor) {
             return res.status(404).json({ status: 404, message: "Visitor not found" });
@@ -59,15 +103,15 @@ router.get("/visitorInfo/:id", async (req, res) => {
 router.get("/GetAllVisitors", async (req, res) => {
     try {
         connectToDB();
-const { id, message } = await getAdminId(req);
+        const { id, message } = await getAdminId(req);
         if (!id) {
             return res.status(401).json({ status: 401, message: message });
         }
-        const FindAdmin=await Admin.findOne({_id:id})
-        const Filter={};
+        const FindAdmin = await Admin.findOne({ _id: id })
+        const Filter = {};
 
-        if(FindAdmin?.Role == "reception" ||FindAdmin?.Role == "user" ){
-            Filter.TowerNo=FindAdmin?.TowerNo
+        if (FindAdmin?.Role == "reception" || FindAdmin?.Role == "user") {
+            Filter.TowerNo = FindAdmin?.TowerNo
         }
         const visitors = await Visitor.find(Filter);
         res.status(200).json({ status: 200, data: visitors });
